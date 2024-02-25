2120cd8c730009f1ffdd9d2d3ad7c7c340c901de
Reece O'Leary <91633093+ThatAnalyst@users.noreply.github.com>
2024-02-25 15:07:14 +0000

    Add files via upload

---
+++ PalindromeCalculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static bool IsPalindrome(int number)
+    {
+        string numStr = number.ToString();
+        char[] charArray = numStr.ToCharArray();
+        Array.Reverse(charArray);
+        string reversedStr = new string(charArray);
+        return numStr == reversedStr;
+    }
+
+    static int FindLargestPalindrome()
+    {
+        int largestPalindrome = 0;
+
+        for (int i = 100; i < 1000; i++)
+        {
+            for (int j = i; j < 1000; j++)
+            {
+                for (int k = j; k < 1000; k++)
+                {
+                    int product = i * j * k;
+                    if (IsPalindrome(product) && product > largestPalindrome)
+                    {
+                        largestPalindrome = product;
+                    }
+                }
+            }
+        }
+
+        return largestPalindrome;
+    }
+
+    static void Main()
+    {
+        int result = FindLargestPalindrome();
+        Console.WriteLine(result);
+    }
+}
